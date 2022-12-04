8635057c0603d2aa24fc1508d8de8a1afd85c377
Chris Zacny <czacny@gmail.com>
2022-12-04 11:18:21 -0800

    chore: Finished day04 part 2. Hooray!

--- day04/main.py
+++ day04/main.py
@@ -22,27 +22,57 @@ def determine_if_range_contained_in_another_range(range1: "tuple[int, int]", ran
             return False
     return True
 
+def determine_if_any_overlap_in_range(range1: "tuple[int, int]", range2: "tuple[int, int]") -> bool:
+    list1 = [a for a in range(range1[0], range1[1] + 1)]
+    list2 = [a for a in range(range2[0], range2[1] + 1)]
+    for v in list1:
+        if v in list2:
+            return True
+    return False
 
-def calculate_answer(data: "list[str]") -> int:
+
+def get_range_tuples_from_line(line: str) -> "tuple[tuple[int], tuple[int]]":
+    pairs = line.split(",")
+    pair_1_left_number = int(pairs[0].split("-")[0])
+    pair_1_right_number = int(pairs[0].split("-")[1])
+    pair_2_left_number = int(pairs[1].split("-")[0])
+    pair_2_right_number = int(pairs[1].split("-")[1])
+
+    left_tuple = (pair_1_left_number, pair_1_right_number)
+    right_tuple = (pair_2_left_number, pair_2_right_number)
+    return (left_tuple, right_tuple)
+
+
+def calculate_answer_part_one(data: "list[str]") -> int:
     """
     2-4,6-8
     """
     answer = 0
     for line in data:
-        pairs = line.split(",")
-        pair_1_left_number = int(pairs[0].split("-")[0])
-        pair_1_right_number = int(pairs[0].split("-")[1])
-        pair_2_left_number = int(pairs[1].split("-")[0])
-        pair_2_right_number = int(pairs[1].split("-")[1])
-
-        left_tuple = (pair_1_left_number, pair_1_right_number)
-        right_tuple = (pair_2_left_number, pair_2_right_number)
+        ranges = get_range_tuples_from_line(line)
 
         # Calcluate if the left is in the right
-        if determine_if_range_contained_in_another_range(left_tuple, right_tuple):
+        if determine_if_range_contained_in_another_range(ranges[0], ranges[1]):
             answer += 1
             continue
-        if determine_if_range_contained_in_another_range(right_tuple, left_tuple):
+        if determine_if_range_contained_in_another_range(ranges[1], ranges[0]):
+            answer += 1
+            continue
+    return answer
+
+def calculate_answer_part_two(data: "list[str]") -> int:
+    """
+    2-4,6-8
+    """
+    answer = 0
+    for line in data:
+        ranges = get_range_tuples_from_line(line)
+
+        # Calcluate if the left is in the right
+        if determine_if_any_overlap_in_range(ranges[0], ranges[1]):
+            answer += 1
+            continue
+        if determine_if_any_overlap_in_range(ranges[1], ranges[0]):
             answer += 1
             continue
     return answer
@@ -50,7 +80,7 @@ def calculate_answer(data: "list[str]") -> int:
 
 class TestHarness(unittest.TestCase):
 
-    def test_basic_answer(self):
+    def test_basic_answer_part_one(self):
         data = """2-4,6-8
         2-3,4-5
         5-7,7-9
@@ -59,7 +89,18 @@ class TestHarness(unittest.TestCase):
         2-6,4-8
         """
         lines = sanitize_data(data)
-        self.assertEqual(2, calculate_answer(lines))
+        self.assertEqual(2, calculate_answer_part_one(lines))
+
+    def test_basic_answer_part_two(self):
+        data = """2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+        """
+        lines = sanitize_data(data)
+        self.assertEqual(4, calculate_answer_part_two(lines))
 
     def test_determine_if_range_contained_in_another_range(self):
         r1 = (2, 4)
@@ -79,7 +120,7 @@ def main():
     with open("input.dat", "r") as f_hdl:
         str_data = f_hdl.read()
         sanitized_data = sanitize_data(str_data)
-        print(calculate_answer(sanitized_data))
+        print(calculate_answer_part_two(sanitized_data))
         
 
 if __name__ == "__main__":
