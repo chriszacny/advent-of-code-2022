9f1d862e0db58192334b6e22407ed90fd7844e46
Chris Zacny <c.zacny@f5.com>
2022-12-05 22:11:39 -0800

    chore: day 06 part one and two complete

---
+++ day06/main.py
@@ -0,0 +1,74 @@
+import unittest
+
+
+START_OF_PACKET_MARKER_LENGTH = 4
+START_OF_MESSAGE_MARKER_LENGTH = 14
+
+
+def check_string_chars_unique(chars: str, num_to_check: int) -> bool:
+    test = set(chars)
+    if len(test) == num_to_check:
+        return True
+    return False
+
+
+def get_location_of_first_marker(buffer: str, marker_length: int) -> int:
+    for i in range(len(buffer)):
+        # TODO Check for overflow
+        slice_to_check = buffer[i: i + marker_length]
+        if check_string_chars_unique(slice_to_check, marker_length):
+            return i + marker_length
+    return None
+
+
+class TestHarness(unittest.TestCase):
+
+    def test_check_string_chars_unique(self):
+        t = "qqas"
+        self.assertEqual(False, check_string_chars_unique(t, START_OF_PACKET_MARKER_LENGTH))
+        u = "qasd"
+        self.assertEqual(True, check_string_chars_unique(u, START_OF_PACKET_MARKER_LENGTH))
+
+    def test_elven_protocol_sop_marker(self):
+        datastream_buffer1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+        self.assertEqual(7, get_location_of_first_marker(datastream_buffer1, START_OF_PACKET_MARKER_LENGTH))
+
+        datastream_buffer2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+        self.assertEqual(5, get_location_of_first_marker(datastream_buffer2, START_OF_PACKET_MARKER_LENGTH))
+
+        datastream_buffer3 = "nppdvjthqldpwncqszvftbrmjlhg"
+        self.assertEqual(6, get_location_of_first_marker(datastream_buffer3, START_OF_PACKET_MARKER_LENGTH))
+
+        datastream_buffer4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+        self.assertEqual(10, get_location_of_first_marker(datastream_buffer4, START_OF_PACKET_MARKER_LENGTH))
+
+        datastream_buffer5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+        self.assertEqual(11, get_location_of_first_marker(datastream_buffer5, START_OF_PACKET_MARKER_LENGTH))
+
+    def test_elven_protocol_som_marker(self):
+        datastream_buffer1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+        self.assertEqual(19, get_location_of_first_marker(datastream_buffer1, START_OF_MESSAGE_MARKER_LENGTH))
+
+        datastream_buffer2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+        self.assertEqual(23, get_location_of_first_marker(datastream_buffer2, START_OF_MESSAGE_MARKER_LENGTH))
+
+        datastream_buffer3 = "nppdvjthqldpwncqszvftbrmjlhg"
+        self.assertEqual(23, get_location_of_first_marker(datastream_buffer3, START_OF_MESSAGE_MARKER_LENGTH))
+
+        datastream_buffer4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+        self.assertEqual(29, get_location_of_first_marker(datastream_buffer4, START_OF_MESSAGE_MARKER_LENGTH))
+
+        datastream_buffer5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+        self.assertEqual(26, get_location_of_first_marker(datastream_buffer5, START_OF_MESSAGE_MARKER_LENGTH))
+
+def main():
+    inputData = "jghhttcmttdwwfjjjpqjqllwvwffswwqmwmddvndvdrrcwrccfncfcgcjjpjrjzjggczgglhllbzlztlzzswwbqqrvrvqrqcrclrccrbrpbbzhbbzvbbwrwswqwqqgmmgqmqmdqdhdmmfnndpdvvdvdbbmgmqgqgqmqrrsbsrrzcrzrcrvrddpqdpdwwsvshvshvshszhshddjllbjljjwhjwhwppbssdccvzzmvvwrrrhmmbdbggqhgqglglgrrdbdjddjvddzpznzmnnpgnppnznzvzbzvvdtdwdttnzzdpzzqjzjzllhglghgttcbtcbtbjbssqcqtcqtqvttvpvqvggbsszjsjqjllfgfqqhllvsvmmjhhbdbzzrwzzggdfgdgcddgmmjnjhhhgvgwghgfffclffdmmmlclzclzzqgqjqzqwzqzbbbljlplnlrlqlqlhlvlmvvlwltwtqwwznndpdttjjrllpptnngjnnzppjhpjhpjpjmjpmjpptzppjpsprpbpmmshmsmqqnhnjhnjnccdnnqhhgfgmgnmnvvtwvttcbtbnbcnnhjjnpphjpjppshsppmrmrbrlrzlzjlzzvssvfffsjfsjshshppchppprpcclhlmmgwwsddgbdggcqqhsqqbdbzzdffmpmvmpptppjrprrgvvgrrztrtllnblbffpgprpqqwcwjwcjcbbjgbbsgsbgbgwbwrrbcrcjcnnvlnlmnnwcncbcrrgcrggtqqgbbqsqhsqslswsdswscwssbqbppczzpmmnjnfjfpjpqpddpbbgrrnqrnqqnwqnntsnsdnddvhhgrgmmgzmgmwgmwwmfwwnqnjjgqghqhhpzpmpbmpbbqvvjjlvvddcnnglnnhpnhhjghgrgjrjqqgtqggmzzbdbssbpssrjsrsbbpzzsgzzpcpwwfgwfgfccslsmlltlnttpvvcbvbqqqqvlvplvvmnntstffvhvvwfftltjjtftbftfqqnbbmnmqqwbbldbldbddtldtldldbllsggdqgdgfgtggglmmgfgcfgccgttgdgdvgglhhvbbnzbzvbzzvtvntvntvvwgvgcgrrmbmnmttzlzgllpmlpmmscsncnnnfrrvqvmvsvsrvvbvpbbvsvssdnnmvnnnggpmpbbqsqwssjwwtnwwhbwwmcwwjfwfhfvvsnsqqwmmrfmrmcrchhpqprpzpmpzzqqmqbqjjbffjnnhphdpdjjbcjcsjcjcdcrddscsgsqgsqggcscrssscvcncfccptcccjgjqqlppjbpjpgpzpgpqqzwqwcwnwccmnnnwrnrttjqtjtllbslsrlrggjrrjwrwmrrcbbnddzcdzcchqqrhhcrcrlrmrcchcpcmcrrlqltlhttdhhjrrzllwjjfvvzccmwwtlwwhpwwssfdsfdsfsddwbbfsfqfwqffhqfqvqwwhfhrrwhrwhrwrzrwrzwwbsblslzzrwzwdwnwznnbsnsbbgffjppvqppbmmdffgghrrchrhfhthqthqhvvrcvrrfvfbvffmmbjmjbbwhhvqvbvjjfzjzvvwnvncvvdssfffjfhjhbbdhdlllmrlmrllbtltbbhnnczzwjzwjwllvtvmvpvvmmfnmnwmwjwrrqtqlltwllwrllsvvdrvrnvrnnsjnjfnnmpnpgnglnggbnbmmcmddhrdrjdjtthssdbssjtjvttqwqppwbpblljnlnljlfjffvddtzzqfzqqjttqhhhvppghpghpghghjggsrrczzqvvbjbbbglldcdrdqdpqpwpffnjnhnthhtlhhrdrcrwrvrlvrlvrvprpccmffqzffdbdhhwjjgmgzzqhqnqsstltjjzszpsppdpnddrnrmrllhtwnlhgzgvsfjfmgfcnnzqhbfztnzhnctmjjhvzrhjcptzqqtstlmrgnbcpnctjgswhfcmtzgsndfzdlqsrlcjrssmjndgrzgvtgfjwtcqwnzmrgtgngcwcwzvttqcdwqspzldfmzgmfclwgqvvvqhhhswpdjlbpjpppdljbdjrbblbrtftdsmvmfpftdlvhdphzbcwwwjvmsjczbtblwbtszmbcmpbdqnwcgcltbdzbsvtjhlqgrsgqzztqrvmswtzgvsjslgvjcvhqftdcmwqwhgwrmrpfdqvfqhczlztrhjqlppchgspfjwzvfsncdhgnlnnshwsbvqmqwtldtmshhqpqbdzqlwfvvbbzwqvlvqdwcpbtrsrhmrjnzmqjttthhbbdjwtzhwjcvhzrtgfwltqhmzwtqbgjjpphlfwfhgctmwpqrmngfrfmbmcqpqcrsmzhjzdzbppbfwpgdtdjvjdvbwfcbddjpjbgtrjtddlpvnppcwhbbgpqgmcrwmfsvqspdvctvljbhcgnllzdpjmjdqwflrfqrsfqnhcsbtszjfmqvjwmcsghwcssbnzzpsggbmpdqrlctrcrmbzlnnqcfmrpfwsnjcggtqncwddsjjzwvlnfpwjlrgwrrvfhgtjwqlblnsnrddjqqtwtzrvfqfqcjhbwtlmfsfgpzgtdddtlvqlqgjwpprpzvdhvlfjbqqflbnvgsgblfpcqprlqrhrrddjjftbmfgghrrrhmttmnfzgmrzwcscnsmdnnddsbdjswhwmjfjnfvjbtcqjlflzjsqqhldcdsbjttmvmcdbwrfwdlllbbjhbmdhnfgwmmbpbsrmqptppqzwtncnwwsjjrchgpvdcsspfqpczmsqfrgvfgsnhfmplrnhzddlbvnthltpwbbzdwwnmhmllwfphhfpnghgdzlzgpbvsphbhvfmcvqpqvdsjjbnbsdvccbmgwdbgsnhpgfshzzznjdbngsrqpwhqjqdfsnmngzznwgvqrtjmzcmbbqjgjcdjdqbmdrgvqflsstqhgsgpfnmzvrdgztlzlhvdjdcslwbgldwjwftvczvtbpdtdqtqshqrbpvpgbfbtnnlmzrhdtsjdlllbtwqgcfphssqmszmbllnbpwvhfdllwtcbqccmbtscmsvppjjrcpswgtzgvhblvbmcddhhgqghbtwzffscsvzgwjdfldccbcfptpmmfbwqzlqhdrcdvhpnwvddqcrwwlmtgvrcvlbvtblhhmhdrvvnpmrdqsgjdrfprfcmtfsbrmsglfjcnjwvpjqlptrbmrqcrdfccmfvhqzrmwqbbmtmjrbnmvzdbwcfpwjzrjrhzrncpwptswhnrgsdpdfjqcjhnvvftgzdccpsgdqcqzfvbtftrzljqmjbgmmlrdlvpwqddmdhzsslzlnvfrblfzfpwtvhpwhmqmbzvchndbzfswtmvcprhlssmncfdqcpzjczptptgdzpjvrqtrlcgbhlqqwvnsrrvtllldbhztgnlmjbmqmgcpcbwtthnhwghnqgvqtnnltbzslmmbjbhqtrmqbgccphjsgwbtstvjnpjjqsdwdsgcrjgznntsgvlrgzmbnzdmbghrmtscppgmztfbsqczzvhsmjmmclhrcbgnvjbgffgbrhrdwccplpdfzcljgfntjlzmsqrwrmdqbclffbfpscddcfsbpsnnphjvjwsfvbprsgpcnbrblvvqfvngbhgmmglzzfmmpnnrphwrvqnmffwpsmmrlhmnsdvbdgjlrjmsdzjltgmjfplrrfmgbhzltzzfpwtqcqwbgsgwfbmntzsvtqqtnrhbtbnshfmwwzbvtsqmtsfssrcvgngvvhjlcnfsvltpsdjspgmmrqttcsltjzqglpspmgrnbrtnbtnjzprqmtfhgczjrlqdhsjqjbhpnwhrffmhvqfmlzcpfflptgqwthfzvspbjjdwmmbnttbztzpnjmlstgmgqbptdncqgbdbdnqwslwhfrdfvmbbqlzhdptpfrhnvcchpddngslzzrhsrwclpccbqhcscbzcpdtwmppvrpjnnjfgrswndtzprjnsvvdwwhhbcsglnwwptptdzgsmbwppdrhwpqhzlgfcsqtfzvqdvcsbtbqvtfvwlcdrwttgmwhbjlqphclqfzmlb"
+    print(get_location_of_first_marker(inputData, START_OF_MESSAGE_MARKER_LENGTH))
+        
+
+if __name__ == "__main__":
+    main()
+
+
+# if __name__ == '__main__':
+#    unittest.main()
